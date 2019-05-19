9ed385fdf03225248985ea3ca7fab13d202ced4b
Jainil10 <jgand039@uottawa.ca>
2019-05-19 19:29:26 -0400

    Added Room Status

---
+++ app/src/main/java/com/home_automation/mealabs/android_app/RoomStatus.java
@@ -0,0 +1,13 @@
+package com.home_automation.mealabs.android_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RoomStatus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_room_status);
+    }
+}
